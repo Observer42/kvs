5406a6d8f4136ec19b6221ef8bb54bfc7711def8
Observer42 <yishengxu@outlook.com>
2019-10-31 01:07:36 +0800

    Project 4, part 7: Evaluating other thread pools and engines

--- benches/thread_pool.rs
+++ benches/thread_pool.rs
@@ -5,71 +5,143 @@ use std::time::Duration;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use tempfile::TempDir;
 
-use kvs::thread_pool::{SharedQueueThreadPool, ThreadPool};
+use kvs::thread_pool::{RayonThreadPool, SharedQueueThreadPool, ThreadPool};
 use kvs::{KvStore, KvsClient, KvsEngine, KvsServer, SledKvsEngine};
 
 fn read_queued_kvstore(c: &mut Criterion) {
+    read_general_bench::<KvStore, SharedQueueThreadPool, Box<dyn Fn() -> KvStore>>(
+        c,
+        Box::new(generate_kvstore),
+        "read_queued_kvstore",
+    );
+}
+
+fn write_queued_kvstore(c: &mut Criterion) {
+    write_general_bench::<KvStore, SharedQueueThreadPool, Box<dyn Fn() -> KvStore>>(
+        c,
+        Box::new(generate_kvstore),
+        "write_queued_kvstore",
+    );
+}
+
+fn read_rayon_kvstore(c: &mut Criterion) {
+    read_general_bench::<KvStore, RayonThreadPool, Box<dyn Fn() -> KvStore>>(
+        c,
+        Box::new(generate_kvstore),
+        "read_rayon_kvstore",
+    );
+}
+
+fn write_rayon_kvstore(c: &mut Criterion) {
+    write_general_bench::<KvStore, RayonThreadPool, Box<dyn Fn() -> KvStore>>(
+        c,
+        Box::new(generate_kvstore),
+        "write_rayon_kvstore",
+    );
+}
+
+fn read_rayon_sledkvengine(c: &mut Criterion) {
+    read_general_bench::<SledKvsEngine, RayonThreadPool, Box<dyn Fn() -> SledKvsEngine>>(
+        c,
+        Box::new(generate_sled_engine),
+        "read_rayon_sledkvengine",
+    );
+}
+
+fn write_rayon_sledkvengine(c: &mut Criterion) {
+    write_general_bench::<SledKvsEngine, RayonThreadPool, Box<dyn Fn() -> SledKvsEngine>>(
+        c,
+        Box::new(generate_sled_engine),
+        "write_rayon_sledkvengine",
+    );
+}
+
+fn read_general_bench<E: KvsEngine, T: ThreadPool, F: Fn() -> E>(
+    c: &mut Criterion,
+    generate_kv: F,
+    group_name: &'static str,
+) {
     let threads = [1, 2, 4, 6, 8];
-    let mut group = c.benchmark_group("read_queued_kvstore");
+    let mut group = c.benchmark_group(group_name);
     for size in threads.iter() {
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            let temp_dir = TempDir::new().unwrap();
-            let kv_store = KvStore::open(temp_dir).unwrap();
+            let engine = generate_kv();
             for i in 1000..2000 {
-                kv_store.set(i.to_string(), "value".to_string()).unwrap();
+                engine.set(i.to_string(), "value".to_string()).unwrap();
             }
             let addr: SocketAddr = "127.0.0.1:4000".parse().unwrap();
-            let thread_pool = SharedQueueThreadPool::new(*size).unwrap();
+            let thread_pool: T = ThreadPool::new(*size).unwrap();
 
-            let server = KvsServer::init(kv_store, addr, thread_pool).unwrap();
+            let server = KvsServer::init(engine, addr, thread_pool).unwrap();
             server.start();
-            thread::sleep(Duration::from_secs(1));
 
-            let client_thread_pool = SharedQueueThreadPool::new(1000).unwrap();
+            let client_thread_pool: T = ThreadPool::new(1000).unwrap();
             b.iter(|| {
                 for num in 1000..2000 {
                     client_thread_pool.spawn(move || {
                         let mut client = KvsClient::init(&addr).unwrap();
                         let key = num.to_string();
-                        let res = client.get(key.clone());
+                        let res = client.get(key);
                         assert!(res.is_ok());
                         assert_eq!(res.unwrap(), Some("value".to_string()));
                     });
                 }
             });
+            thread::sleep(Duration::from_secs(1));
             server.stop_server();
         });
     }
 }
 
-fn write_queued_kvstore(c: &mut Criterion) {
+fn write_general_bench<E: KvsEngine, T: ThreadPool, F: Fn() -> E>(
+    c: &mut Criterion,
+    generate_kv: F,
+    group_name: &'static str,
+) {
     let threads = [1, 2, 4, 6, 8];
-    let mut group = c.benchmark_group("write_queued_kvstore");
+    let mut group = c.benchmark_group(group_name);
     for size in threads.iter() {
         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            let temp_dir = TempDir::new().unwrap();
-            let kv_store = KvStore::open(temp_dir).unwrap();
+            let engine = generate_kv();
             let addr: SocketAddr = "127.0.0.1:4000".parse().unwrap();
-            let thread_pool = SharedQueueThreadPool::new(*size).unwrap();
+            let thread_pool: T = ThreadPool::new(*size).unwrap();
 
-            let server = KvsServer::init(kv_store, addr, thread_pool).unwrap();
+            let server = KvsServer::init(engine, addr, thread_pool).unwrap();
             server.start();
-            thread::sleep(Duration::from_secs(1));
 
-            let client_thread_pool = SharedQueueThreadPool::new(*size).unwrap();
+            let client_thread_pool: T = ThreadPool::new(*size).unwrap();
             b.iter(|| {
                 for num in 1000..2000 {
                     client_thread_pool.spawn(move || {
                         let mut client = KvsClient::init(&addr).unwrap();
                         let key = num.to_string();
-                        assert!(client.set(key.clone(), "value".to_string()).is_ok());
+                        assert!(client.set(key, "value".to_string()).is_ok());
                     });
                 }
             });
+            thread::sleep(Duration::from_secs(1));
             server.stop_server();
         });
     }
 }
 
-criterion_group!(benches, write_queued_kvstore, read_queued_kvstore);
+fn generate_kvstore() -> KvStore {
+    let temp_dir = TempDir::new().unwrap();
+    KvStore::open(temp_dir).unwrap()
+}
+
+fn generate_sled_engine() -> SledKvsEngine {
+    let temp_dir = TempDir::new().unwrap();
+    SledKvsEngine::open(temp_dir).unwrap()
+}
+
+criterion_group!(
+    benches,
+    write_queued_kvstore,
+    read_queued_kvstore,
+    read_rayon_kvstore,
+    write_rayon_kvstore,
+    read_rayon_sledkvengine,
+    write_rayon_sledkvengine
+);
 criterion_main!(benches);
