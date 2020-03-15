8bd26e6a18bd4a352b8f5be78a9ee0b3ebd1a47f
Observer42 <yishengxu47@gmail.com>
2020-03-15 20:50:30 +0800

    Change to single file KVS

--- src/engine/kv_store.rs
+++ src/engine/kv_store.rs
@@ -3,6 +3,7 @@ use std::fs::{create_dir_all, File, OpenOptions};
 use std::io::prelude::*;
 use std::io::{BufReader, BufWriter, SeekFrom};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
 use serde::{Deserialize, Serialize};
@@ -11,7 +12,6 @@ use crate::engine::{try_add_engine_type, EngineType};
 use crate::{KvsEngine, KvsError, Result};
 
 const COMPACTION_THRESHOLD: u32 = 10_000;
-const FILE_SIZE_LIMIT: usize = 1_048_576;
 
 #[derive(Serialize, Deserialize)]
 pub(crate) enum Cmd {
@@ -21,18 +21,13 @@ pub(crate) enum Cmd {
 
 #[derive(Copy, Clone)]
 struct LogIndex {
-    file_index: usize,
     offset: u64,
     len: u64,
 }
 
 impl LogIndex {
-    fn new(file_index: usize, offset: u64, len: u64) -> Self {
-        Self {
-            file_index,
-            offset,
-            len,
-        }
+    fn new(offset: u64, len: u64) -> Self {
+        Self { offset, len }
     }
 }
 
@@ -87,12 +82,11 @@ impl KvsEngine for KvStore {
 
 struct KvStoreInner {
     path: PathBuf,
+    epoch: u64,
     key_index: BTreeMap<String, LogIndex>,
-    index_scanned: usize,
-    active_reader: BufReader<File>,
-    active_writer: BufWriter<File>,
-    wal_logs: Vec<BufReader<File>>,
-    active_redundant: u32,
+    reader: BufReader<File>,
+    writer: BufWriter<File>,
+    redundant: u32,
 }
 
 impl KvStoreInner {
@@ -110,47 +104,48 @@ impl KvStoreInner {
                 files.push(entry);
             }
         }
-        files.sort_by_key(|item| item.file_name());
 
-        let mut wal_logs = vec![];
-        for entry in &files {
-            let file = File::open(entry.path())?;
-            if entry.file_name().to_str().unwrap().contains(".log") {
-                wal_logs.push(BufReader::new(file));
-            }
-        }
+        let mut log_files = files
+            .iter()
+            .filter_map(|entry| {
+                let file_name = entry.file_name().to_str().unwrap().to_owned();
+                if file_name.ends_with(".log") {
+                    let prefix = file_name.trim_end_matches(".log");
+                    let epoch = u64::from_str(prefix).ok()?;
+                    Some((entry, epoch))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
 
-        let (active_reader, active_writer) = if files.is_empty()
-            || !files
-                .last()
-                .unwrap()
-                .file_name()
-                .to_str()
-                .unwrap()
-                .contains("wal_active")
-        {
-            let path = log_dir.join("wal_active".to_string());
-            let writer = File::create(&path)?;
-            let reader = File::open(path)?;
-            (BufReader::new(reader), BufWriter::new(writer))
-        } else {
-            let path = files.last().unwrap().path();
+        log_files.sort_by_key(|&(_, epoch)| epoch);
+
+        let (reader, writer, epoch) = if let Some((entry, epoch)) = log_files.last() {
+            let path = entry.path();
             let writer = OpenOptions::new().append(true).open(&path)?;
             let reader = File::open(path)?;
-            (BufReader::new(reader), BufWriter::new(writer))
+            (BufReader::new(reader), BufWriter::new(writer), *epoch)
+        } else {
+            let path = log_dir.join("0.log".to_string());
+            let writer = File::create(&path)?;
+            let reader = File::open(path)?;
+            (BufReader::new(reader), BufWriter::new(writer), 0)
         };
 
         let key_index = BTreeMap::new();
 
-        Ok(Self {
+        let mut store = Self {
             path: log_dir,
+            epoch,
             key_index,
-            index_scanned: wal_logs.len() + 1,
-            active_reader,
-            active_writer,
-            wal_logs,
-            active_redundant: 0,
-        })
+            reader,
+            writer,
+            redundant: 0,
+        };
+
+        store.import_log()?;
+        Ok(store)
     }
 
     fn set(&mut self, key: String, value: String) -> Result<()> {
@@ -165,16 +160,6 @@ impl KvStoreInner {
                 Cmd::Rm(_) => Ok(None),
             }
         } else {
-            while self.index_scanned != 0 {
-                self.import_next_log()?;
-                if let Some(&log_index) = self.key_index.get(&key) {
-                    let cmd = self.read_from_log(log_index)?;
-                    match cmd {
-                        Cmd::Set(_, val) => return Ok(Some(val)),
-                        Cmd::Rm(_) => return Ok(None),
-                    }
-                }
-            }
             Ok(None)
         }
     }
@@ -183,72 +168,41 @@ impl KvStoreInner {
         if self.key_index.contains_key(&key) {
             self.append_log(Cmd::Rm(key.clone()), key)
         } else {
-            while self.index_scanned != 0 {
-                self.import_next_log()?;
-                if let Some(&log_index) = self.key_index.get(&key) {
-                    let cmd = self.read_from_log(log_index)?;
-                    if let Cmd::Rm(_) = cmd {
-                        return Err(KvsError::KeyNotFound);
-                    } else {
-                        return self.append_log(Cmd::Rm(key.clone()), key);
-                    }
-                }
-            }
             Err(KvsError::KeyNotFound)
         }
     }
 
     fn append_log(&mut self, cmd: Cmd, key: String) -> Result<()> {
-        let offset = self.active_writer.seek(SeekFrom::End(0))?;
-        serde_json::to_writer(&mut self.active_writer, &cmd)?;
-        self.active_writer.flush()?;
-        let new_offset = self.active_writer.seek(SeekFrom::End(0))?;
+        let offset = self.writer.seek(SeekFrom::End(0))?;
+        serde_json::to_writer(&mut self.writer, &cmd)?;
+        self.writer.flush()?;
+        let new_offset = self.writer.seek(SeekFrom::End(0))?;
 
-        let file_index = self.wal_logs.len();
-        let log_index = LogIndex::new(file_index, offset, new_offset - offset);
+        let log_index = LogIndex::new(offset, new_offset - offset);
 
         //trigger compaction if necessary: too much redundant records or active_file too large
-        if let Some(old_index) = self.key_index.insert(key, log_index) {
-            if old_index.file_index == self.wal_logs.len() {
-                self.active_redundant += 1;
-                if self.active_redundant > COMPACTION_THRESHOLD {
-                    self.minor_compact()?
-                }
+        if let Some(_) = self.key_index.insert(key, log_index) {
+            self.redundant += 1;
+            if self.redundant > COMPACTION_THRESHOLD {
+                self.compact()?
             }
-        }
-        if offset as usize > 10 * FILE_SIZE_LIMIT {
-            self.minor_compact()?
         }
         Ok(())
     }
 
     fn read_from_log(&mut self, log_index: LogIndex) -> Result<Cmd> {
-        let reader = if log_index.file_index == self.wal_logs.len() {
-            &mut self.active_reader
-        } else {
-            &mut self.wal_logs[self.index_scanned]
-        };
+        let reader = &mut self.reader;
         reader.seek(SeekFrom::Start(log_index.offset))?;
 
         let take = reader.take(log_index.len);
         serde_json::from_reader(take).map_err(|e| e.into())
     }
 
-    fn import_next_log(&mut self) -> Result<()> {
-        if self.index_scanned == 0 {
-            return Ok(());
-        }
-        self.index_scanned -= 1;
-        let reader = if self.index_scanned == self.wal_logs.len() {
-            &mut self.active_reader
-        } else {
-            &mut self.wal_logs[self.index_scanned]
-        };
-
-        reader.seek(SeekFrom::Start(0))?;
+    fn import_log(&mut self) -> Result<()> {
+        self.reader.seek(SeekFrom::Start(0))?;
         let mut cur_pos = 0;
         let mut local_index = BTreeMap::new();
-        let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Cmd>();
+        let mut stream = serde_json::Deserializer::from_reader(&mut self.reader).into_iter::<Cmd>();
 
         while let Some(cmd) = stream.next() {
             let key = match cmd? {
@@ -256,7 +210,7 @@ impl KvStoreInner {
                 Cmd::Rm(key) => key.clone(),
             };
             let new_pos = stream.byte_offset() as u64;
-            local_index.insert(key, LogIndex::new(self.index_scanned, cur_pos, new_pos - cur_pos));
+            local_index.insert(key, LogIndex::new(cur_pos, new_pos - cur_pos));
             cur_pos = new_pos;
         }
 
@@ -267,46 +221,31 @@ impl KvStoreInner {
         Ok(())
     }
 
-    fn minor_compact(&mut self) -> Result<()> {
-        let active_path = self.path.join("wal_active".to_string());
+    fn compact(&mut self) -> Result<()> {
+        let temp_path = self.path.join("temp");
+        let mut new_writer = BufWriter::new(File::create(&temp_path)?);
         let mut new_key_index = self.key_index.clone();
 
-        let new_active_path = self.path.join("new_active");
-        let mut new_active_writer = BufWriter::new(File::create(&new_active_path)?);
-
-        let prev_active_index = self.wal_logs.len();
-        let mut cur_index = self.wal_logs.len();
         let mut offset = 0;
         for (_, log_index) in new_key_index.iter_mut() {
-            if log_index.file_index == prev_active_index {
-                if offset + log_index.len as usize >= FILE_SIZE_LIMIT {
-                    // save file as wal_##.log and create new active_file
-                    drop(new_active_writer);
-                    let next_log_path = self.path.join(format!("wal_{:05}.log", cur_index));
-                    std::fs::rename(&new_active_path, &next_log_path)?;
-                    self.wal_logs.push(BufReader::new(File::open(next_log_path)?));
-
-                    new_active_writer = BufWriter::new(File::create(&new_active_path)?);
-                    cur_index += 1;
-                    offset = 0;
-                }
-
-                let cmd = self.read_from_log(*log_index)?;
-                serde_json::to_writer(&mut new_active_writer, &cmd)?;
-
-                log_index.file_index = cur_index;
-                log_index.offset = offset as u64;
-                offset += log_index.len as usize;
-            }
+            let cmd = self.read_from_log(*log_index)?;
+            serde_json::to_writer(&mut new_writer, &cmd)?;
+            log_index.offset = offset as u64;
+            offset += log_index.len as usize;
         }
-        new_active_writer.flush()?;
-        drop(new_active_writer);
-        std::fs::rename(new_active_path, &active_path)?;
+        new_writer.flush()?;
+        drop(new_writer);
 
-        self.active_redundant = 0;
+        let old_path = self.path.join(format!("{}.log", self.epoch));
+        let _ = std::fs::remove_file(old_path);
+        self.epoch += 1;
+        let new_path = self.path.join(format!("{}.log", self.epoch));
+        std::fs::rename(temp_path, &new_path)?;
+
+        self.redundant = 0;
         self.key_index = new_key_index;
-        self.active_reader = BufReader::new(File::open(&active_path)?);
-        self.active_writer = BufWriter::new(OpenOptions::new().append(true).open(active_path)?);
+        self.reader = BufReader::new(File::open(&new_path)?);
+        self.writer = BufWriter::new(OpenOptions::new().append(true).open(new_path)?);
 
         Ok(())
     }
